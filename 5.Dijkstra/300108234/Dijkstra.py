7edbe56057100a2af3e1d1d9e56231c04f839a08
halimabzn <43480203+halimabzn@users.noreply.github.com>
2018-12-06 20:44:07 -0500

    Update Dijkstra.py

--- 5.Dijkstra/300108234/Dijkstra.py
+++ 5.Dijkstra/300108234/Dijkstra.py
@@ -33,4 +33,16 @@ def find_lowest_cost_node(costs):
             lowest_cost = cost 
             lowest_cost_node = node
     return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)  
   ```
