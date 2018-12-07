df0d18be6178063cd081d8a0b29e10085bfcc6f9
GOLDEN <43043248+Futureseven@users.noreply.github.com>
2018-12-07 14:20:20 -0500

    Update Dijsktra.py

--- 5.Dijkstra/300113127/Dijsktra.py
+++ 5.Dijkstra/300113127/Dijsktra.py
@@ -1,7 +1,5 @@
 ´´´
-graph = {}
-graph["you"] = ["alice", "bob", "claire"]
-graph["start"] = {}
+graph = {"start": {}}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
 graph["a"] = {}
@@ -13,25 +11,31 @@ graph["fin"] = {}
 infinity = float("inf")
 costs = {"a": 6, "b": 2, "fin": infinity}
 parents = {"a": "start", "b": "start", "fin": None}
-processed = []
+
+
 def find_lowest_cost_node(costs):
     lowest_cost = float("inf")
     lowest_cost_node = None
-    node = find_lowest_cost_node(costs)
-    while node is not None:
-        cost = costs[node]
-        neighbors = graph[node]
-        for n in neighbors.keys():
-            new_cost = cost + neighbors[n]
-            if costs[n] > new_cost:
-                costs[n] = new_cost
-                parents[n] = node
-        processed.append(node)
-        node = find_lowest_cost_node(costs)
     for node in costs:
         cost = costs[node]
         if cost < lowest_cost and node not in processed:
             lowest_cost = cost
             lowest_cost_node = node
     return lowest_cost_node
+
+
+processed = []
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print(costs["fin"])
   ´´´
